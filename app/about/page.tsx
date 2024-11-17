0b0e163f679a966f2f63befdce4b608004503a70
Almir Muminovic <projectalmir@gmail.com>
2024-11-17 15:16:04 +0100

    Testing routing in Next by creating components

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  console.log("Server Component")
+  return (
+    <div>
+        <h1>Name</h1>
+        <p>Surname</p>
+    </div>
+  )
+}
+
+export default About
---
+++ app/components/test.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from 'react'
+
+export const Test = () => {
+    console.log("Client Side Component")
+  return (
+    <h1>This is TEST component, used as client side component</h1>
+  )
+}
