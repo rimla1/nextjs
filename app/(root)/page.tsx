17a5aac054cd4ecde5574133288d7bd447e7ed3f
Almir Muminovic <projectalmir@gmail.com>
2024-11-18 10:59:24 +0100

    Fetching data inside of a Server Component without useEffect

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image';
 import { Test } from '../components/test';
 
-
-export default function Home() {
-  console.log('Simple console log from SERVER component!');
+const Home = async () => {
+  const response = await fetch('https://api.deezer.com/search?q=eminem');
+  if (!response.ok) throw new Error('Failed to fetch data');
+  const songs = await response.json();
 
   return (
-    <>
-      <h1 className='text-3xl'>Hello from Next</h1>
-      <Test />
-    </>
+    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {songs?.data?.map((song: { id: number; title: string }) => (
+        <div
+          key={song.id}
+          className="bg-gray-800 text-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-200"
+        >
+          <h3 className="text-lg font-bold truncate">{song.title}</h3>
+          <p className="text-sm text-gray-400">ID: {song.id}</p>
+        </div>
+      ))}
+    </div>
   );
-}
+};
+
+export default Home;
+
